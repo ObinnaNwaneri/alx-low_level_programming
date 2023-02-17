c2121381bf6c4d9908042f3f2b189da286d53cad
obinnanwaneri <nwaneriobinna3@gmail.com>
2023-02-16 17:27:37 -0800

    yeah

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+/**
+ * main - using the puts function to print to the standard output.
+ * Return: 0
+ */
+
+int main(void)
+{
+puts("\"Programming is like building a multilingual puzzle\n");
+return (0);
+}
