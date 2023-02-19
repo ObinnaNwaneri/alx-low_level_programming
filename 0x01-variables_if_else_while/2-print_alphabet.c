310c2ea2fee19100d4c471136dcd22035a072863
obinnanwaneri <nwaneriobinna3@gmail.com>
2023-02-18 23:58:44 -0800

    yeah

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,15 @@
+#include <studio.h>
+
+/**
+ * main - a program that prints out the alphabet using putchar
+ * Return: 0
+ */
+
+int main(void)
+{
+	for (int x = 'A'; x <= 'Z'; x++)
+	{
+		putchar (tolower(x));
+	}
+	return (0);
+}
