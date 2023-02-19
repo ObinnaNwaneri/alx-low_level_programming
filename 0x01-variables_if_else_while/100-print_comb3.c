f6a5091239fff3cb49126d3351913dba5e0307e6
obinnanwaneri <nwaneriobinna3@gmail.com>
2023-02-19 10:29:07 -0800

    advanced

---
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -0,0 +1,31 @@
+#include <stdlib.h>
+#include <stdio.h>
+
+/**
+ * main - prints all possible combinations of single-digit numbers
+ * Return: 0
+ */
+
+int main(void)
+{
+	int number[10] = {'0', '1', '2', '3', '4', '5', '6', '7', '8', '9'};
+
+	int n;
+
+	for (n = 0; n < 10; n++)
+	{
+		for (n = 0; n < 10; n++)
+		{
+			if (number[n] != number[n])
+			{
+				putchar(number[n]);
+				putchar(',');
+				putchar(' ');
+			}
+		}
+	}
+
+	putchar ('\n');
+
+	return (0);
+}
