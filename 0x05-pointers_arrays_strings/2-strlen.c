dc99d605d0e8698fc37584ebfb7c9bd0b67c8d2c
obinnanwaneri <nwaneriobinna3@gmail.com>
2023-02-27 14:17:17 -0800

    yeah

---
+++ 0x05-pointers_arrays_strings/2-strlen.c
@@ -0,0 +1,19 @@
+#include "main.h"
+
+/**
+ * _strlen - a function that returns the length of a string
+ *
+ * @s: input string
+ *
+ * Return: length
+ */
+
+int _strlen(char *s);
+{
+	int length;
+
+	length = sizeof(*s) / sizeof(*s[0]);
+
+	return (length);
+
+}
