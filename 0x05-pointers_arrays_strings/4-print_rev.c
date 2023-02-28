d87e723da7edc107e083bf1bd081f0d08b06fb05
obinnanwaneri <nwaneriobinna3@gmail.com>
2023-02-28 06:27:28 -0800

    yeah

--- 0x05-pointers_arrays_strings/4-print_rev.c
+++ 0x05-pointers_arrays_strings/4-print_rev.c
@@ -2,6 +2,7 @@
 
 /**
  * print_rev - prints a string, followed by a new line.
+ *
  * @s: input string to print.
  */
 
@@ -9,14 +10,10 @@ void print_rev(char *s)
 {
 	int i;
 
-	while (*s[i] != '\0')
-	{
-		i++;
-	}
-	i--;
-	while (*s[i] != '\0')
-	{
-		_putchar(*s[i--]);
-	}
+	len = _strlen(char *s);
+
+	for (i = len; i >= 0; i--)
+		_putchar(*s[i]);
+
 	_putchar('\n');
 }
