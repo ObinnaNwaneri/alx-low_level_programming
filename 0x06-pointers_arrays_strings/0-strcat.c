c773c21ea06a57731cd2a7bedde1017e2916e163
obinnanwaneri <nwaneriobinna3@gmail.com>
2023-03-01 23:15:12 -0800

    yeah

--- 0x06-pointers_arrays_strings/0-strcat.c
+++ 0x06-pointers_arrays_strings/0-strcat.c
@@ -11,22 +11,18 @@
 
 char *_strcat(char *dest, char *src)
 {
-int i = 0, j = 0;
+	int i, j;
 
-	while (dest[i] != '\0')
+	i = 0;
+
+	for (j = 0; dest[j] != '\0'; j++)
+		;
+
+	while (src[i] != '\0')
 	{
+		dest[j + i] = src[i];
 		i++;
 	}
 
-	while (src[j] != '\0')
-	{
-		dest[i] = src[j];
-
-		i++;
-		j++;
-	}
-
-	dest[i] = '\0';
 	return (dest);
-
 }
