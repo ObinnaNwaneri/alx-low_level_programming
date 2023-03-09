608a38de102e13780243d7c33002ad28b4a44680
obinnanwaneri <nwaneriobinna3@gmail.com>
2023-03-08 16:06:38 -0800

    yeah

--- 0x08-recursion/0-puts_recursion.c
+++ 0x08-recursion/0-puts_recursion.c
@@ -9,14 +9,14 @@
  * On error, -1 is returned, and errno is set appropriately.
  */
 
-void _puts_recursion(char *s)	
+void _puts_recursion(char *s)
 {
 	if (*s)
 	{
 		_putchar(*s);
 		_puts_recursion(s + 1);
 	}
-	
+
 	else
 		_putchar('\n');
 }
