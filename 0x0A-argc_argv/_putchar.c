59b7fb81dabc48cca1a796b050a853aa167dd4ec
obinnanwaneri <nwaneriobinna3@gmail.com>
2023-03-12 09:53:11 -0700

    yeah

--- 0x0A-argc_argv/_putchar.c
+++ 0x0A-argc_argv/_putchar.c
@@ -3,10 +3,12 @@
 /**
  * _putchar - puts a char
  *
+ * @c: input argument
+ *
  * Return: the character to write to stdout.
  */
 
 int _putchar(char c)
 {
-	return(write(1, &c, 1));
+	return (write(1, &c, 1));
 }
