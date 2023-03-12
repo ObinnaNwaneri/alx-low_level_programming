752728d0975d21719f711d55436d852a6a39f3ab
obinnanwaneri <nwaneriobinna3@gmail.com>
2023-03-12 09:19:57 -0700

    yeah

---
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -0,0 +1,17 @@
+#include "main.h"
+
+/**
+ * main - a function that prints the name of it's program
+ *
+ * @argc: total number of arguments
+ * @argv[]: string of arguments
+ *
+ * Return: 0
+ */
+
+int main(int argc, char* argv[])
+{
+	_putcar("%d\n", argv[0]);
+
+	return 0;
+}
---
+++ 0x0A-argc_argv/main.h
@@ -0,0 +1,7 @@
+#ifndef main_h
+#define main_h
+
+int _putchar(int c);
+int main(int argc, char* argv[]);
+
+#endif
