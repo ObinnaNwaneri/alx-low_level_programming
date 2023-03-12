b6d4e19d6577d8d5db608c51ae0b6652cad7b6be
obinnanwaneri <nwaneriobinna3@gmail.com>
2023-03-12 09:32:31 -0700

    yeah

--- 0x0A-argc_argv/0-whatsmyname.c
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -5,14 +5,14 @@
  * main - a function that prints the name of it's program
  *
  * @argc: total number of arguments
- * @argv[]: string of arguments
+ * @argv: string of arguments
  *
  * Return: 0
  */
 
-int main(int argc, char* argv[])
+int main(int argc, char *argv[])
 {
 	printf("%s\n", argv[0]);
 
-	return 0;
+	return (0);
 }
